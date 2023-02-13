2d6ecca3379ac711453bd012e2359ddce597b996
annared <annared1982@gmail.com>
2023-02-13 17:32:54 +0200

    user2

--- src/index.js
+++ src/index.js
@@ -1,47 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
+import { App } from 'components/App';
 import './index.css';
 
-import user from './user.json';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  Profile(user)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-function Profile({ username, tag, location, avatar, stats }) {
-  return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
-      </div>
-
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers} </span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
