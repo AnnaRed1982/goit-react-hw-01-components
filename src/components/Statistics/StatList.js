ff5afc3426be6f55a9f461c6da87312e83f0ccb4
annared <annared1982@gmail.com>
2023-02-14 20:58:18 +0200

    c2 proptype

--- src/components/Statistics/StatList.js
+++ src/components/Statistics/StatList.js
@@ -1,15 +1,27 @@
+import PropTypes from 'prop-types';
+
 export default function StatList({ stats }) {
   return (
     <ul className="stat-list">
-      {stats.map(stat => (
-        <li key={stat.id}>
-          <span className="label">{stat.label}</span>
-          <span className="percentage">{stat.percentage}</span>
-        </li>
-      ))}
+      {stats.map(stat => {
+        return (
+          <li key={stat.id}>
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}</span>
+          </li>
+        );
+      })}
     </ul>
   );
 }
+
+StatList.propTypes = {
+  stats: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  })).isRequired,
+};
 
 // import StatItem from './StatItem';
 
