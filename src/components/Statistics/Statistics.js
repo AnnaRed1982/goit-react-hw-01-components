117d26f65dd9068eb1d9031e38e4ae093cadcafb
annared <annared1982@gmail.com>
2023-02-16 15:19:52 +0200

    comp2 css

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
-import StatList from './StatList';
+import css from 'components/Statistics/Statistics.module.css'
+// import StatList from './StatList';
 
 export default function Statistics({ title, stats }) {
   return (
-    <section className="statistics">
-      {{ title } && <h2 className="title">{title}</h2>}
+    <section className={css.statistics}>
+      {{ title } && <h2 className={css.title}>{title}</h2>}
 
-      <StatList stats={stats} />
+      <ul className={css.stat_list}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li className={css.item} key={id}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* <StatList stats={stats} /> */}
     </section>
   );
 }
 
 Statistics.propTypes = {
   title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ).isRequired,
 };
