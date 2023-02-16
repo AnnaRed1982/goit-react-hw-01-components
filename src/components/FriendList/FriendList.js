dcf6f8610e6d36220da3b3f4de30e846400c2c4c
annared <annared1982@gmail.com>
2023-02-16 17:25:56 +0200

    comp3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
-import css from 'components/FriendList/FriendList.module.css'
+import css from 'components/FriendList/FriendList.module.css';
 // import FriendListItem from './FriendListItem';
 
 export default function FriendList({ friends }) {
   return (
-    <ul className={css["friend-list"]}>
+    <ul className={css['friend-list']}>
       {friends.map(({ id, avatar, name, isOnline }) => {
         return (
           <li className={css.item} key={id}>
-            <span className={css.status}>{isOnline}</span>
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+            <img
+              className={css.avatar}
+              src={avatar}
+              alt="User avatar"
+              width="48"
+            />
             <p className={css.name}>{name}</p>
           </li>
         );
